2d641731bf954a710e15fc305017389b9b719477
JIGAR-CHHEDA <jigarchheda3@gmail.com>
2018-05-20 23:32:57 +0530

    Emit newMessages on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,10 @@ var socket = io();
             socket.on('connect' , function(){
                 console.log('connected to server');
 
-                socket.emit('createMessage' , {
-                    from:"jiggy",
-                    text:"helllooo",
-                })
+                // socket.emit('createMessage' , {
+                //     from:"jiggy",
+                //     text:"helllooo",
+                // })
             })
 
             
